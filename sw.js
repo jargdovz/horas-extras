6232335f9d650f01924d26dd221cbc96ddd5f8c8
jargdovz <thehousedeeper@gmail.com>
2026-06-20 04:20:31 +0200

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,16 +1,30 @@
-const CACHE = 'horas-extras-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon192.png', './icon512.png'];
+const CACHE = 'horas-extras-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon192.png',
+  './icon512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
